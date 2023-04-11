65e13803f0bc14f68ffb0124264ffa6e9b68abb3
kzamorae <kzaplicaciones@gmail.com>
2023-04-11 12:39:12 -0500

    actualizacion11423

--- AppVehiculoKzamora/src/app/app-routing.module.ts
+++ AppVehiculoKzamora/src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VehiculosComponent } from './paginas/Vehiculos/Vehiculos.component';
 import { VehiculoDetalleComponent } from './paginas/VehiculoDetalle/VehiculoDetalle.component';
+import { HomeComponent } from './paginas/home/home/home.component';
 
 const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
   {
     path:'vehiculos',
     component:VehiculosComponent
